af166692058d23f7ec2a41c66fd11a21702defe8
andimatter <hermydrea@web.de>
2023-04-21 11:45:20 +0200

    ex4

--- ex4/graph_clique.py
+++ ex4/graph_clique.py
@@ -12,14 +12,14 @@ import sys
 
 import time
 
-graph_file = "Weighted_pairwise-gene-clusters.txt"
+# graph_file = "Weighted_pairwise-gene-clusters.txt"
 # graph_file = "graph_eng.txt"
 # graph_file = "graph_short.txt"
 # graph_file = "graph_mid.txt"
 # graph_file = "graph_7.txt"
 # graph_file = "9606.protein.physical.links.v11.5.txt"
 
-# graph_file = sys.argv[1]
+graph_file = sys.argv[1]
 
 if graph_file[-4:] != ".txt":
     graph_file += ".txt"
@@ -28,15 +28,15 @@ start = time.time()
 
 gtable = pd.read_csv(graph_file, sep='\s+', header=None, usecols=[0,1])
 
-print("read time", time.time() - start)
+# print("read time", time.time() - start)
 
 
 #----------------
 id_dict = {} # look up index given id
 index_dict = {} # look up id given index
-unique_indices = 0
-adjacency_lists = []
-degrees = []
+unique_indices = 0 # count unique node names
+adjacency_lists = [] 
+degrees = [] # dont actually need this here, could use for optimising
 
 start = time.time()
 
@@ -44,45 +44,44 @@ for index, row in gtable.iterrows():
     vert_A, vert_B = row
     
     try:
-        ind_A = id_dict[vert_A][0]
+        ind_A = id_dict[vert_A][0] # find index for protein A
     except:
-        id_dict[vert_A] = [unique_indices, 0]       
+        id_dict[vert_A] = [unique_indices, 0] # if new protein, assing new index
         ind_A = unique_indices
         index_dict[ind_A] = vert_A
-        adjacency_lists.append([])
-        degrees.append(0)
-        unique_indices += 1
+        adjacency_lists.append([]) # new protein, needs new adjacency list
+        degrees.append(0) # and degree counter
+        unique_indices += 1 # new protein found
         
     try:
-        ind_B = id_dict[vert_B][0]
+        ind_B = id_dict[vert_B][0] # find index for protein B
     except:
-        id_dict[vert_B] = [unique_indices, 0]
+        id_dict[vert_B] = [unique_indices, 0] # if new protein, assing new index
         ind_B = unique_indices
         index_dict[ind_B] = vert_B
-        adjacency_lists.append([])
-        degrees.append(0)
-        unique_indices += 1
+        adjacency_lists.append([]) # new protein, needs new adjacency list
+        degrees.append(0) # and degree counter
+        unique_indices += 1 # new protein found
         
-    #print(ind_A, ind_B)
-        
+    # now just register that A and B are connected
     adjacency_lists[ind_A].append(ind_B)
     adjacency_lists[ind_B].append(ind_A)
     
     degrees[ind_A] += 1
     degrees[ind_B] += 1       
 
-print("loop time", time.time() - start)
+# print("loop time", time.time() - start)
 
 #------------
 start = time.time()
 
-plot = False
+plot = False # postprocessing data save keeping for timings sake
 
 if plot:
     adjacency_file = open("adjacency.txt", 'w+')
 
 for list_ in adjacency_lists:
-    list_.sort()
+    list_.sort() # sort adjacency lists (not super sure if the numpy functions actually are faster if sorted but we can hope)
     if plot:
         adjacency_file.write(str(list_))
         adjacency_file.write("\n")
@@ -90,131 +89,72 @@ for list_ in adjacency_lists:
 if plot:
     adjacency_file.close()
 
-print("sort time", time.time() - start)
+# print("sort time", time.time() - start)
 #------------------
 
-# cliques of size 2
+
 
 start = time.time()
 
 ## >  <
-# ignore this, my work notebook doesnt have these symbols >:[
+# ignore this, my work laptop doesnt have these symbols >:[
 
+# cliques of size 2
 clique_2 = []
 
-for k in range(unique_indices):
-    
+for k in range(unique_indices):   
     for j in range(k, unique_indices):
         if j in adjacency_lists[k]:
             clique_2.append([k,j])
-        # else: print("NO")
-        
-# print(len(clique_2))
-# print(unique_indices)    
 
-# print(adjacency_lists)   
+         
 
 i = 2
 
 condition = True
 
-S_lists = [clique_2]
+S_lists = [clique_2] # first bunch of cliques
 
-max_deg = max(degrees)
-print("max deg", max_deg)
 
 stop = False
-
-
-
 while not stop:
     
-    i += 1
-    
-    if i == 10:
-        stop = True
-    
-    
-    # print(i) 
+    i += 1     
     
     # S_i := ∅;
-    S_i = []
-    
-    
-    
-    S_i1 = S_lists[i-1-2]
-    # print("S_i1", S_i1)
-    
-    if len(S_i1[-1]) > max_deg + 1:
-        break # cant have cliques of bigger than max number of connections on one node + 1
-    
-    # for j := 1 to |S_i-1|
-    
-    last_N = []
-    
-    
+    S_i = []      
+    S_i1 = S_lists[i-1-2] # this is S_i-1
+
+    # for j := 1 to |S_i-1|   
     for j in range(1, len(S_i1)):
         
         # for k := j+1 to |S_i-1|
         for k in range(j+1, len(S_i1)):
             
-            # print(S_i1)
-            # print(S_i1[j])
-            # print(S_i1[k])
-            
-            # print(np.in1d(S_i1[j], S_i1[k], assume_unique=True))
-            
             # T := S_i-1[j] ∩ S_i-1[k];
             T = np.array(S_i1[j])[np.in1d(S_i1[j], S_i1[k], assume_unique=True)]
-
-            # print(T)
             
             if len(T) == i-2:
-                # print(S_i1[j], S_i1[k])
                 N = np.union1d(S_i1[j], S_i1[k])
                 notN = np.setxor1d(S_i1[j], S_i1[k])
-                # print(len(N), notN)
-                
-                # if notN[0] == 435:
-                # print("N ", N)
-                # print("NOT N", notN)
-                # print("adj", adjacency_lists[notN[1]])
                 
                 # check if N is clique
                 # N is clique if all elements connected
                 # N made of two sub-cliques
                 # just have to check if not-overlapping elements are connected to each other
-                if notN[0] in adjacency_lists[notN[1]]:
-                    # print("S_i here" , S_i)
+                if notN[0] in adjacency_lists[notN[1]]:                   
+                    S_i.append(N)
                     
-                    already_in = False
-                    
-                    for last_N in S_i:
-                        # print("ALL", np.all(np.isin(N, last_N)))
-                        if np.all(np.isin(N, last_N)): 
-                            already_in = True
-                    if not already_in:
-                        S_i.append(N)
-                                       
-            # print("\n j break")
-        # print("\n k break \n") 
-        
-    # print("\n i break", S_i)
+    # delete doubled cliques from list
+    S_i = np.unique(S_i, axis=0)
     
-    stop =  len(S_i) == 0
-    
+    stop =  len(S_i) == 0 # stop if no new cliques were found    
     if stop:
         break
     
-    S_lists.append(S_i)
+    S_lists.append(S_i) # register new cliques
     
-    
-# for list_ in S_lists:
-    # print(list_)
-   
-print("cliq time", time.time() - start)
-    
-print("Maximum cliques: ")
+print(len(S_lists[-1][0]))
 for clique in S_lists[-1]:
-    print(clique)
+    #print(clique)
     print([index_dict[k] for k in clique])
--- ex4/graph_degree.py
+++ ex4/graph_degree.py
@@ -14,14 +14,12 @@ import sys
 import time
 
 
-
-
-graph_file = "graph_short.txt"
+# graph_file = "graph_short.txt"
 # graph_file = "graph_mid.txt"
 # graph_file = "graph_7.txt"
 # graph_file = "9606.protein.physical.links.v11.5.txt"
 
-# graph_file = sys.argv[1]
+graph_file = sys.argv[1]
 
 if graph_file[-4:] != ".txt":
     graph_file += ".txt"
@@ -30,7 +28,7 @@ start = time.time()
 
 gtable = pd.read_csv(graph_file, sep=" ", header=None, usecols=[0,1])
 
-print("read time", time.time() - start)
+# print("read time", time.time() - start)
 
 # print(gtable)
 
@@ -48,25 +46,24 @@ for index, row in gtable.iterrows():
     vert_A, vert_B = row
     
     try:
-        ind_A = id_dict[vert_A][0]
+        ind_A = id_dict[vert_A][0] # find index for protein A
     except:
-        id_dict[vert_A] = [unique_indices, 0]
+        id_dict[vert_A] = [unique_indices, 0] # if new protein, assing new index
         ind_A = unique_indices
-        adjacency_lists.append([])
-        degrees.append(0)
-        unique_indices += 1
+        adjacency_lists.append([]) # new protein, needs new adjacency list
+        degrees.append(0) # and degree counter
+        unique_indices += 1 # new protein found
         
     try:
-        ind_B = id_dict[vert_B][0]
+        ind_B = id_dict[vert_B][0] # find index for protein B
     except:
-        id_dict[vert_B] = [unique_indices, 0]
+        id_dict[vert_B] = [unique_indices, 0] # if new protein, assing new index
         ind_B = unique_indices
-        adjacency_lists.append([])
-        degrees.append(0)
-        unique_indices += 1
+        adjacency_lists.append([]) # new protein, needs new adjacency list
+        degrees.append(0) # and degree counter
+        unique_indices += 1 # new protein found
         
-    #print(ind_A, ind_B)
-        
+    # now just register that A and B are connected        
     adjacency_lists[ind_A].append(ind_B)
     adjacency_lists[ind_B].append(ind_A)
     
@@ -74,23 +71,19 @@ for index, row in gtable.iterrows():
     degrees[ind_B] += 1
         
 
-print("loop time", time.time() - start)
+# print("loop time", time.time() - start)
 
-#print(hash_lists)
 
-#degrees = pd.DataFrame.from_dict(id_dict, orient="index")[1].to_numpy()
-
-#print(degrees) 
-#print(unique_indices)
-
-degree_binned, bin_edges = np.histogram(degrees, bins=np.arange(1, max(degrees)+2))
+degree_binned, bin_edges = np.histogram(degrees, bins=np.arange(1, max(degrees)+2)) # count how often which degree occurs
 
 
 # PUT IN FOR HANDIN
-# for k in range(len(degree_binned)):
-    # print(degree_binned[-k-1], end=" ")
+for k in range(len(degree_binned)):
+    print(degree_binned[-k-1], end=" ")
 
 
+
+### END OF RELEVANT CODE
 start = time.time()
 
 plot = False
@@ -112,7 +105,7 @@ if plot:
 # print(adjacency_lists)
 
 
-print("sort time", time.time() - start)
+# print("sort time", time.time() - start)
 
 #print(np.sort(np.array(adjacency_lists)))
 
@@ -122,6 +115,7 @@ print("sort time", time.time() - start)
 
 
 
+# used for saving data
 if plot:
     
     rev_deg_counts = degree_binned[::-1]
